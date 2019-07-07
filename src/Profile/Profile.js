53a4267e9da3e700f7b917c53724949deb465dda
nuttawuto <nuttawut.ongkanupap@sivantos.com>
2019-07-07 21:01:22 +0800

    banner format

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -4,14 +4,16 @@ import './Profile.css';
 function Profile() {
   return (
     <div className="banner px-0">
-    <div className="profile col-md-12 px-0">
-        <img className="profile-image img-fluid px-0" id="profile-big" src={process.env.PUBLIC_URL + '/photo/profile2-big.jpg'} alt="profile" />
-        <img className="col-md-12 profile-image img-fluid px-0" id="profile-small" src={process.env.PUBLIC_URL + '/photo/profile2-small.jpg'} alt="profile" />
-    </div>
+      <div className="profile col-md-12 px-0">
+          <img className="profile-image img-fluid px-0" id="profile-big" src={process.env.PUBLIC_URL + '/photo/profile2-big.jpg'} alt="profile" />
+          <img className="col-md-12 profile-image img-fluid px-0" id="profile-small" src={process.env.PUBLIC_URL + '/photo/profile2-small.jpg'} alt="profile" />
+      </div>
 
       <div className="title col-md-6 d-flex justify-content-center">
         <p>Hey, I'm Scarlett</p>
-        <i className="fa fa-rotate-90"></i>
+      </div>  
+
+      {/* <div className="resume ">
         <div className="icon" title="Resume">
             <a className="icon" href={process.env.PUBLIC_URL + '/download/profile-big.jpg'} download>
               <img src={process.env.PUBLIC_URL + '/icon/doc.svg'} alt="doc"></img>
@@ -22,8 +24,7 @@ function Profile() {
               <img src={process.env.PUBLIC_URL + '/icon/pdf.svg'} alt="pdf"></img>
             </span>
         </div>
-        
-      </div>
+      </div> */}
     </div>
   );
 }
