601b72c515dfa109c9a162263634b3542f8c02d9
nuttawuto <nuttawut.ongkanupap@sivantos.com>
2019-07-07 22:19:10 +0800

    update banner image to backgroud image

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -3,12 +3,7 @@ import './Banner.css';
 
 function Banner() {
   return (
-    <div id="banner-div" className="px-0">
-      {/* <div className="profile col-md-12 px-0">
-          <img className="profile-image img-fluid px-0" id="profile-big" src={process.env.PUBLIC_URL + '/photo/profile2-big.jpg'} alt="profile" />
-          <img className="col-md-12 profile-image img-fluid px-0" id="profile-small" src={process.env.PUBLIC_URL + '/photo/profile2-small.jpg'} alt="profile" />
-      </div> */}
-
+    <div id="banner-div" className="row px-0">
       <div className="title col-md-6 d-flex justify-content-center">
         <p>Hey, I'm Scarlett</p>
       </div>  
