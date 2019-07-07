2e883d7f1f16f2e959707266694309b6c26b50f8
nuttawuto <nuttawut.ongkanupap@sivantos.com>
2019-07-07 20:46:17 +0800

    Banner

---
+++ src/Profile/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile() {
+  return (
+    <div className="banner px-0">
+    <div className="profile col-md-12 px-0">
+        <img className="profile-image img-fluid px-0" id="profile-big" src={process.env.PUBLIC_URL + '/photo/profile2-big.jpg'} alt="profile" />
+        <img className="col-md-12 profile-image img-fluid px-0" id="profile-small" src={process.env.PUBLIC_URL + '/photo/profile2-small.jpg'} alt="profile" />
+    </div>
+
+      <div className="title col-md-6 d-flex justify-content-center">
+        <p>Hey, I'm Scarlett</p>
+        <i className="fa fa-rotate-90"></i>
+        <div className="icon" title="Resume">
+            <a className="icon" href={process.env.PUBLIC_URL + '/download/profile-big.jpg'} download>
+              <img src={process.env.PUBLIC_URL + '/icon/doc.svg'} alt="doc"></img>
+            </a>
+        </div>
+        <div className="icon" title="Resume">
+            <span className="icon" href={process.env.PUBLIC_URL + '/download/profile-big.jpg'} download>
+              <img src={process.env.PUBLIC_URL + '/icon/pdf.svg'} alt="pdf"></img>
+            </span>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Profile from './Profile/Profile'
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Profile />, document.getElementById('profile'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
