96f9c0270469eb229c342d15713a6df1bba8eb1c
nuttawuto <nuttawut.ongkanupap@sivantos.com>
2019-07-08 21:17:24 +0800

    adjust

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -5,7 +5,7 @@ function Banner() {
   return (
     <div id="banner-div" className="row px-0">
       <div className="title col-md-6 d-flex justify-content-center">
-        <p>Hey, I'm Scarlett</p>
+        <h2>Hey, I'm Scarlett</h2>
       </div>  
     </div>
   );
