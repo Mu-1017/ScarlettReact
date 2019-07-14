102e2d4e6107d94ab03064f479ffc4acf1d103b3
nuttawuto <nuttawut.ongkanupap@sivantos.com>
2019-07-14 21:08:28 +0800

    Add empty experience section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SectionHeader from './SectionHeader/SectionHeader'
 import Banner from './Banner/Banner'
 import Profile from './Profile/Profile'
 import Professional from './Professional/Professional'
-import PlaceHolder from './PlaceHolder'
+import Experience from './Experience/Experience'
 import Contact from "./Contact/Contact"
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
       <Professional/>
 
       <SectionHeader name="Experience" desc="Here's what I've done so far..."/>
-      <PlaceHolder/>
+      <Experience/>
 
       <SectionHeader name="Contact" desc="I am available for freelance projects and full-time employment"/>
       <Contact/>
---
+++ src/Experience/Experience.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Experience.css';
+
+function Experience() {
+    return (
+        <div id="main-div" className="container">
+            <div className="container">
+                <h2>Careers</h2>
+                <div className="row">
+
+                </div>
+                <div className="row">
+
+                </div>
+            </div>
+            <hr id="linebreak"/>
+            <div>
+                <h2>Education</h2>
+            </div>
+        </div>
+    );
+}
+export default Experience;
