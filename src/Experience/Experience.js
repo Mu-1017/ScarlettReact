b470419a91e185218f68196bdb3a5b4b980fd6a4
nuttawuto <nuttawut.ongkanupap@sivantos.com>
2019-07-14 22:01:19 +0800

    update experience section

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -5,17 +5,28 @@ function Experience() {
     return (
         <div id="main-div" className="container">
             <div className="container">
-                <h2>Careers</h2>
-                <div className="row">
-
+                <div className="row d-flex justify-content-center">
+                    <div className="company col-4 text-left">
+                        <h4>ST Electronics</h4>
+                        <p>Jan 2013	- Jun 2017</p>
+                    </div>
+                    <div className="col-8 text-left">
+                        <h4>Senior Software Engineer</h4>
+                        <p>Worked as a Senior Devloper</p>
+                    </div>
                 </div>
                 <div className="row">
 
                 </div>
             </div>
             <hr id="linebreak"/>
-            <div>
-                <h2>Education</h2>
+            <div className="container">
+                <div className="row">
+
+                </div>
+                <div className="row">
+
+                </div>
             </div>
         </div>
     );
