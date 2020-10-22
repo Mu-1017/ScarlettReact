603e164da97403b9761fa63da1d3fc305fbcd5cc
Tong Mu <tong.mu@sivantos.com>
2020-10-22 16:02:41 +0800

    Updates

---
+++ precache-manifest.9553d0fafced6d8cf58252948bcf73ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde181578891573cace70e9c0b7ce176",
+    "url": "/ScarlettReact/index.html"
+  },
+  {
+    "revision": "fe45f8c887a3833cc300",
+    "url": "/ScarlettReact/static/css/main.02e4ab72.chunk.css"
+  },
+  {
+    "revision": "1ca33b5b5c95cd679d4b",
+    "url": "/ScarlettReact/static/js/2.229e5af2.chunk.js"
+  },
+  {
+    "revision": "fe45f8c887a3833cc300",
+    "url": "/ScarlettReact/static/js/main.cad39847.chunk.js"
+  },
+  {
+    "revision": "c499a1409fe05ea31cf8",
+    "url": "/ScarlettReact/static/js/runtime~main.5e389313.js"
+  },
+  {
+    "revision": "481e4546868b6a31181600f74b60a11b",
+    "url": "/ScarlettReact/static/media/profile2-big.481e4546.jpg"
+  },
+  {
+    "revision": "beaf203be7a7bbb96f88db82866a0a46",
+    "url": "/ScarlettReact/static/media/profile2-small.beaf203b.jpg"
+  }
+]);
